4929d5a62464002d353adbff4fd5c9caccffc1a7
Mayank Kumar <computergeekone@gmail.com>
2022-11-18 21:20:40 -0500

    First part of HW

---
+++ Homework2 .js
@@ -0,0 +1,92 @@
+/*
+for(var i = 0;i<=15;i++){
+    console.log(i);
+}
+
+for(var i = 15;i>=0;i--){
+    console.log(i);
+}
+
+let laps = 0;
+while(laps<=20){
+    console.log("That's another lap!");
+    laps++
+}
+console.log("You did " + laps + " laps");
+
+
+let age = 89;
+for(i=1;i<=100;i++){
+    if(i == age){
+        console.log("This is the correct age");
+    }
+    else{
+        console.log("Age doesn't match");
+    }
+}
+
+for(var i = 0;i<=700;i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+    else{
+        null;
+    }
+}
+
+for(var i = 0;i<=700;i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+    else{
+        null;
+    }
+} */
+
+for(var i = 1900;i<=1949;i++){
+    if(i == 1900){
+        console.log("The Zeppelin was invented in 1900!");
+    }
+    else if(i == 1902){
+        console.log("The Teddy Bear was invented in 1902!");
+    }
+    else if(i == 1903){
+        console.log("The crayon was invented in 1903!");
+    }
+    else if(i == 1910){
+        console.log("The Talking motion Picture was invented in 1910!");
+    }
+    else if(i == 1913){
+        console.log("The Bra was invented in 1913!");
+    }
+    else if(i == 1918){
+        console.log("The Fortune Cookie was invented in 1918!");
+    }
+    else if(i == 1920){
+        console.log("The tommy gun and band-aid were invented in 1920!");
+    }
+    else if(i == 1921){
+        console.log("The Robot was invented in 1921!");
+    }
+    else if(i == 1923){
+        console.log("The Traffic Signal was invented in 1923!");
+    }
+    else if(i == 1929){
+        console.log("The Car Radio was invented in 1929!");
+    }
+    else if(i == 1934){
+        console.log("The game Monopoly was invented in 1934!");
+    }
+    else if(i == 1935){
+        console.log("The Radar was invented in 1935!");
+    }
+    else if(i == 1938){
+        console.log("The Ballpoint Pen was invented in 1938!");
+    }
+    else if(i == 1943){
+        console.log("The Slinky was invented in 1943!");
+    }
+    else{
+        console.log(i);
+    }
+}
